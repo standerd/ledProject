992adf98d363d776a91e6dd4c4d9f90266f7fe63
Dewalds <stander.dewald@gmail.com>
2022-02-13 10:14:44 +0200

    commit on main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
+
+
+class App extends Component{
+
+  //fetch(
+    //"http://192.168.88.130:88/login?username=Dewald&password=password123",
+    //{
+      //method: "POST",
+      //headers: {
+        //"Content-Type": "application/json"
+      //},
+      //body: JSON.stringify({
+        //username: "Dewald",
+        //password: "password123"
+      //})
+   // }
+ // )
+
+
+render(){
+
+  //** user login handler
+function ledOn (e){
+
+  let userName = e.target.name;
+
+  const data = new FormData();
+  data.append("username", userName);
+  data.append("password", "password123");
+
+  fetch(
+    "http://192.168.88.130:88/login",
+    {
+      method:"POST",
+      body: data
+    }
+  )
+    .then(resData => {
+      console.log(resData);
+    })
+    .catch(err => {
+      
+    });
+};
+
+function ledOff (e){
+
+  fetch(
+    "http://192.168.88.130:88/login?username=Lourika&password=password123",
+    {
+      method:"POST"
+    }
+  )
+    .then(resData => {
+      console.log(resData);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <button name="Dewald" onClick={ledOn}>Switch On</button>
+       <button name="Lourika" onClick={ledOn}>Switch Off</button>
+       
       </header>
     </div>
   );
 }
+}  
+
+  
 
 export default App;
+
+
